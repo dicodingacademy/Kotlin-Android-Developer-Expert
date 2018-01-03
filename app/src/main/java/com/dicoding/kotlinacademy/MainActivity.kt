7833296fa7adaeab02f3ccf333f33e7203c5c6f1
Nur Rohman <ennurr31@gmail.com>
2018-01-03 14:08:18 +0700

    adding setText code with Kotlin

--- app/src/main/java/com/dicoding/kotlinacademy/MainActivity.kt
+++ app/src/main/java/com/dicoding/kotlinacademy/MainActivity.kt
@@ -2,11 +2,15 @@ package com.dicoding.kotlinacademy
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textHello = findViewById<TextView>(R.id.text_hello)
+        textHello.text = "Hello Kotlin!"
     }
 }

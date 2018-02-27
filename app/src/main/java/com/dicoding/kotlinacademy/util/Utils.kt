653fe3b98a84d840e82d5a1bd11509521de0a429
Nur Rohman <ennurr31@gmail.com>
2018-02-27 13:44:51 +0700

    remove unused function

--- app/src/main/java/com/dicoding/kotlinacademy/util/Utils.kt
+++ app/src/main/java/com/dicoding/kotlinacademy/util/Utils.kt
@@ -1,14 +1,6 @@
 package com.dicoding.kotlinacademy.util
 
-import android.annotation.SuppressLint
 import android.view.View
-import java.text.SimpleDateFormat
-import java.util.*
-
-@SuppressLint("SimpleDateFormat")
-fun toSimpleString(date: Date?): String? = with(date ?: Date()) {
-    SimpleDateFormat("EEE, dd MMM yyy").format(this)
-}
 
 fun View.visible() {
     visibility = View.VISIBLE

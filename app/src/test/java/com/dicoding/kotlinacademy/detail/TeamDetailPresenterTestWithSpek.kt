9abcdbe94d4d5391e6a911b9809ca1378eefb40e
Nur Rohman <ennurr31@gmail.com>
2018-06-07 14:40:11 +0700

    test TeamDetailPresenter wit spek

--- app/src/test/java/com/dicoding/kotlinacademy/detail/TeamDetailPresenterTestWithSpek.kt
+++ app/src/test/java/com/dicoding/kotlinacademy/detail/TeamDetailPresenterTestWithSpek.kt
@@ -1,9 +1,11 @@
-package com.dicoding.kotlinacademy.teams
+package com.dicoding.kotlinacademy.detail
 
 import com.dicoding.kotlinacademy.TestContextProvider
 import com.dicoding.kotlinacademy.api.ApiRepository
 import com.dicoding.kotlinacademy.api.TheSportDBApi
 import com.dicoding.kotlinacademy.model.TeamResponse
+import com.dicoding.kotlinacademy.teams.TeamsPresenter
+import com.dicoding.kotlinacademy.teams.TeamsView
 import com.google.gson.Gson
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
@@ -14,28 +16,29 @@ import org.junit.runner.RunWith
 import org.mockito.Mockito
 
 @RunWith(JUnitPlatform::class)
-class TeamsPresenterTestWithSpek: Spek({
-    describe("testing teams presenter") {
+class TeamDetailPresenterTestWithSpek: Spek({
+    describe("testing teams detail presenter") {
 
         val gson = Mockito.mock(Gson::class.java)
-        val view = Mockito.mock(TeamsView::class.java)
+        val view = Mockito.mock(TeamDetailView::class.java)
         val teamResponse = Mockito.mock(TeamResponse::class.java)
         val apiRepository = Mockito.mock(ApiRepository::class.java)
 
-        val presenter = TeamsPresenter(view, apiRepository, gson, TestContextProvider())
+        val presenter = TeamDetailPresenter(view, apiRepository, gson, TestContextProvider())
+        val id = "1234"
 
-        on("get team list") {
+        on("get team detail") {
             Mockito.`when`(
                     gson.fromJson(
-                            apiRepository.doRequest(TheSportDBApi.getTeams("Spanish La liga")),
+                            apiRepository.doRequest(TheSportDBApi.getTeamDetail(id)),
                             TeamResponse::class.java)).thenReturn(teamResponse)
-            presenter.getTeamList("Spanish La liga")
+            presenter.getTeamDetail(id)
 
             it("show loading") {
                 Mockito.verify(view).showLoading()
             }
-            it("success get team list") {
-                Mockito.verify(view).showTeamList(teamResponse.teams)
+            it("success get team detail") {
+                Mockito.verify(view).showTeamDetail(teamResponse.teams)
             }
             it("hide loading") {
                 Mockito.verify(view).hideLoading()

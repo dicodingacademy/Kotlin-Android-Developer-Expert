0e526ddc0cde1fe6eedb77836cf894b8006e0877
Nur Rohman <ennurr31@gmail.com>
2018-07-24 14:53:59 +0700

    using uri builder to return uri

--- app/src/main/java/com/dicoding/kotlinacademy/api/TheSportDBApi.kt
+++ app/src/main/java/com/dicoding/kotlinacademy/api/TheSportDBApi.kt
@@ -1,13 +1,30 @@
 package com.dicoding.kotlinacademy.api
 
+import android.net.Uri
 import com.dicoding.kotlinacademy.BuildConfig
 
 object TheSportDBApi {
     fun getTeams(league: String?): String {
-        return BuildConfig.BASE_URL + "api/v1/json/${BuildConfig.TSDB_API_KEY}" + "/search_all_teams.php?l=" + league
+        return Uri.parse(BuildConfig.BASE_URL).buildUpon()
+                .appendPath("api")
+                .appendPath("v1")
+                .appendPath("json")
+                .appendPath(BuildConfig.TSDB_API_KEY)
+                .appendPath("search_all_teams.php")
+                .appendQueryParameter("l", league)
+                .build()
+                .toString()
     }
 
     fun getTeamDetail(teamId: String?): String{
-        return BuildConfig.BASE_URL + "api/v1/json/${BuildConfig.TSDB_API_KEY}" + "/lookupteam.php?id=" + teamId
+        return Uri.parse(BuildConfig.BASE_URL).buildUpon()
+                .appendPath("api")
+                .appendPath("v1")
+                .appendPath("json")
+                .appendPath(BuildConfig.TSDB_API_KEY)
+                .appendPath("lookupteam.php")
+                .appendQueryParameter("id", teamId)
+                .build()
+                .toString()
     }
 }

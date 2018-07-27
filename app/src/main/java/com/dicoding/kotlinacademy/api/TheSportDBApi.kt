d3fcac17ee85450a401d7c1b1b21725e137e6337
Nur Rohman <ennurr31@gmail.com>
2018-07-27 11:32:34 +0700

    take out spek testing framework

--- app/src/main/java/com/dicoding/kotlinacademy/api/TheSportDBApi.kt
+++ app/src/main/java/com/dicoding/kotlinacademy/api/TheSportDBApi.kt
@@ -1,30 +1,13 @@
 package com.dicoding.kotlinacademy.api
 
-import android.net.Uri
 import com.dicoding.kotlinacademy.BuildConfig
 
 object TheSportDBApi {
     fun getTeams(league: String?): String {
-        return Uri.parse(BuildConfig.BASE_URL).buildUpon()
-                .appendPath("api")
-                .appendPath("v1")
-                .appendPath("json")
-                .appendPath(BuildConfig.TSDB_API_KEY)
-                .appendPath("search_all_teams.php")
-                .appendQueryParameter("l", league)
-                .build()
-                .toString()
+        return BuildConfig.BASE_URL + "api/v1/json/${BuildConfig.TSDB_API_KEY}" + "/search_all_teams.php?l=" + league
     }
 
     fun getTeamDetail(teamId: String?): String{
-        return Uri.parse(BuildConfig.BASE_URL).buildUpon()
-                .appendPath("api")
-                .appendPath("v1")
-                .appendPath("json")
-                .appendPath(BuildConfig.TSDB_API_KEY)
-                .appendPath("lookupteam.php")
-                .appendQueryParameter("id", teamId)
-                .build()
-                .toString()
+        return BuildConfig.BASE_URL + "api/v1/json/${BuildConfig.TSDB_API_KEY}" + "/lookupteam.php?id=" + teamId
     }
 }
--- app/src/test/java/com/dicoding/kotlinacademy/api/ApiRepositoryTest.kt
+++ app/src/test/java/com/dicoding/kotlinacademy/api/ApiRepositoryTest.kt
@@ -1,31 +1,19 @@
 package com.dicoding.kotlinacademy.api
 
-import org.jetbrains.spek.api.Spek
-import org.jetbrains.spek.api.dsl.describe
-import org.jetbrains.spek.api.dsl.it
-import org.jetbrains.spek.api.dsl.on
-import org.junit.platform.runner.JUnitPlatform
-import org.junit.runner.RunWith
-import org.mockito.Mockito
+import org.junit.Test
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.verify
+
 
 /**
  * Created by root on 3/1/18.
  */
-@RunWith(JUnitPlatform::class)
-class ApiRepositoryTest : Spek({
-    describe("test do request") {
-
-        val apiRepository = Mockito.mock(ApiRepository::class.java)
+class ApiRepositoryTest {
+    @Test
+    fun testDoRequest() {
+        val apiRepository = mock(ApiRepository::class.java)
         val url = "https://www.thesportsdb.com/api/v1/json/1/search_all_teams.php?l=English%20Premier%20League"
-
-        on("do request") {
-            apiRepository.doRequest(url)
-
-            it("request success") {
-                Mockito.verify(apiRepository).doRequest(url)
-
-            }
-        }
+        apiRepository.doRequest(url)
+        verify(apiRepository).doRequest(url)
     }
-
-})
+}
